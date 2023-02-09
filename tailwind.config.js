0da6900a80de68e9f2b47e8553bffe958a15ac8c
Black Saxon <asantep400@gmail.com>
2023-02-09 00:01:09 +0000

    Initialized tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/*.js'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#e63a16',
+                secondary: '#06379a',
+                accent: '#ffb800',
+            },
+        },
+    },
+    plugins: [],
+};
